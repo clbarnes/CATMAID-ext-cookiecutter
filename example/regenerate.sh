1e25a59a172f6f1a14f0bcea71e2e6a104c68dbf
Chris Barnes <barnesc@janelia.hhmi.org>
2019-04-25 17:23:12 -0400

    fix regenerate

--- example/regenerate.sh
+++ example/regenerate.sh
@@ -5,8 +5,13 @@ set -e
 HERE="$( cd "$(dirname "$0")" ; pwd -P )"
 GIT_BACKUP="$HERE/git_backup"
 TGT="CATMAID-myextension"
-OUTER_HASH=$(git rev-parse --short HEAD)
-OUTER_DIFF=
+
+COMMIT_MSG=$(cat <<-END
+CATMAID-ext-cookiecutter $(git rev-parse --short HEAD)
+
+$(git status -vv)
+END
+)
 
 if [[ -d "$TGT" ]]; then
     mv ${TGT}/.git ${GIT_BACKUP}
@@ -16,8 +21,12 @@ cookiecutter --default-config -f --no-input ../
 
 if [[ -d "$GIT_BACKUP" ]]; then
     mv ${GIT_BACKUP} ${TGT}/.git
+else
+    cd $TGT
+    git init
+    cd $HERE
 fi
 
 cd ${TGT}
 git add .
-git commit -m $(printf "CATMAID-ext-cookiecutter $(git rev-parse --short HEAD)\n\n$(git status -vv)")
+git commit -m "$COMMIT_MSG"
